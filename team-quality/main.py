0e37567f107da6b927854563c07e788682c8b0ae
andrek9 <andre.ambition@gmail.com>
2013-06-17 22:23:43 -0700

    added Handler class and jinja

--- team-quality/main.py
+++ team-quality/main.py
@@ -16,9 +16,26 @@
 #
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystem)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw)) 
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Hi guise! ^.^')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
