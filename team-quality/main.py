d69159ade760766540b1767bf08236169ca970fc
Jonathan Poon <jonathanysp@gmail.com>
2013-06-18 14:12:05 -0700

    add nodejs folder

--- team-quality/main.py
+++ team-quality/main.py
@@ -16,26 +16,29 @@
 #
 import webapp2
 
-import jinja2
+#import jinja2
 import os
+import logging
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystem)
+#jinja_env = jinja2.Environment(loader = jinja2.FileSystem)
 
 class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+	#def write(self, *a, **kw):
+		#self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
 
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw)) 
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw)) 
 
 class MainHandler(Handler):
     def get(self):
         self.response.write('Hi guise! ^.^')
+    def post(self):
+        logging.debug("POST");
 
 class MobileHandler(webapp2.RequestHandler):
     def get(self):
