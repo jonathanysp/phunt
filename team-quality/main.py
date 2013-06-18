30b863001cee05539a850efc27d6da8fec0de7e4
Jonathan Poon <jonathanysp@gmail.com>
2013-06-17 22:14:23 -0700

    add static file path

--- team-quality/main.py
+++ team-quality/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(hello.html)
 
 class MobileHandler(webapp2.RequestHandler):
     def get(self):
