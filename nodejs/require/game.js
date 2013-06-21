c881561ebc0889ab6c6e0b3083b1cb38e4da59a4
Jonathan Poon <jonathanysp@gmail.com>
2013-06-20 22:58:16 -0700

    changes

--- nodejs/require/game.js
+++ nodejs/require/game.js
@@ -193,7 +193,7 @@ exports.isFirst = isFirst;
 var disqualify = function(gameid, userid, tasknum){
 	var g = getGame(gameid);
 	g.scores[userid][parseInt(tasknum)] = 0;
-	g.images[userid][parseInt(tasknum)] = "wwww.http://placehold.it/250x200";
+	g.images[userid][parseInt(tasknum)] = "http://placehold.it/250x200";
 
 	io.sockets.in(gameid).emit("disqualify", {
 		userid: userid,
