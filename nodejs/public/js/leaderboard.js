60ec17ebbf9bf14292f9610d556aa5f1cc38c7bf
Jonathan Poon <jonathanysp@gmail.com>
2013-07-24 22:32:45 +0800

    removed uneeded old parts

--- nodejs/public/js/leaderboard.js
+++
@@ -1,5 +0,0 @@
-window.onload = function () {
-	//tell which game to listen to
-	//register('0', null);
-	//getInfo('0');
-}
--- nodejs_packaged/public/js/leaderboard.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-	addLeaderboardEvents();
-	//tell which game to listen to
-	//register('0', null);
-	//getInfo('0');
-}
